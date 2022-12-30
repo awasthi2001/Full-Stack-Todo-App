b1789f03b706ae4291b807d0f28cf72b54f4b6c0
awasthi2001 <akshanshawasthi013@gmail.com>
2022-12-30 21:45:57 +0530

    working on it

--- controllers/Todo.controllers.js
+++ controllers/Todo.controllers.js
@@ -11,16 +11,7 @@ export const getAllTodo = async(req,res)=>{
     const length = await TodoModel.find({User_Id}).count();
     let page = (req.query.page) -1 || 0 ;
     const limit = (req.query.limit) || length;
-    let filter = req.query.filter
-    filter = {User_Id}
-    if(filter==""){
-        filter = {User_Id};
-    }else if(filter=="true"){
-        filter = {User_Id,status:true};
-    }else if(filter=="false"){
-        filter = {User_Id,status:false};
-    }
-    data = await TodoModel.find({filter}).skip(page*limit).limit(limit).sort({"createdAt":-1})//or skip((page-1)*limit).limit(limit)
+    data = await TodoModel.find({User_Id}).skip(page*limit).limit(limit).sort({"createdAt":-1})//or skip((page-1)*limit).limit(limit)
     return res.status(200).send({
         data : data,
         count : length
